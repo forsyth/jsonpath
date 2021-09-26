7e6f3b685837064c155195f80cfe4dce3263fc9c
Charles Forsyth <charles.forsyth@gmail.com>
2021-09-26 23:03:24 +0100

    inelegant slice parsing

--- path.go
+++ path.go
@@ -17,18 +17,19 @@ type Step struct {
 // started with the IETF drafts, but reverted to a grammar adapted from https://github.com/dchester/jsonpath/blob/master/lib/grammar.js
 // path ::= "$" step*
 // step ::= "." member | ".." member | "[" subscript "]" | ".." "[" subscript "]"
-// member ::= "*" | identifier | expr | integer
+// member ::= "*" | identifier | expr | signed-integer
 // subscript ::= subscript-expression | union-element ("," union-element)
 // subscript-expression ::= "*" | expr | filter
 // union-element ::=  array-index | string-literal | array-slice   // could include identifier?
-// array-index ::= integer
+// array-index ::= signed-integer
 // array-slice ::= start? ":" end? (":" stride?)?
-// start ::= integer | expr
-// end ::= integer | expr
-// stride ::= integer | expr
+// start ::= signed-integer | expr
+// end ::= signed-integer | expr
+// stride ::= signed-integer | expr
 // expr ::= "(" script-expression ")"
 // filter ::= "?(" script-expression ")"
-// integer := "-"? [0-9]+
+// signed-integer := "-"? integer
+// integer ::= [0-9]+
 
 // ParsePath returns the parsed form of the path expression in s, or an error.
 func ParsePath(s string) (Path, error) {
@@ -40,6 +41,8 @@ func (p *parser) lookPath() token {
 	return p.look(p.lexPath())
 }
 
+// path ::= "$" step*
+// step ::= "." member | ".." member | "[" subscript "]" | ".." "[" subscript "]"
 func (p *parser) parsePath() (Path, error) {
 	err := expect(p, '$')
 	if err != nil {
@@ -89,11 +92,7 @@ func (p *parser) parsePath() (Path, error) {
 			}
 			path = append(path, sub)
 		default:
-			// TO DO: make lexeme printable
-			if lx.tok.hasVal() {
-				return nil, fmt.Errorf("unexpected token %v (%v)", lx.tok, lx.val)
-			}
-			return nil, fmt.Errorf("unexpected token %v", lx.tok)
+			return nil, fmt.Errorf("unexpected token %v", lx)
 		}
 	}
 }
@@ -117,52 +116,154 @@ func (p *parser) parseBrackets() (*Step, error) {
 // subscript ::= subscript-expression | union-element ("," union-element)
 // subscript-expression ::= "*" | expr | filter
 // union-element ::=  array-index | string-literal | array-slice   // could include identifier?
-// array-index ::= integer
-// array-slice ::= start? ":" end? (":" step?)?
+// array-index ::= signed-integer
+// array-slice ::= start? ":" end? (":" stride?)?
+//
+// it's easier to accept a list of any both subscript-expressions and union-elements
+// and analyse the value list to see what it is (or if it's an illegal list)
 func (p *parser) parseSubscript() (Op, Val, error) {
-	union := []*Step{} // if it's the sequence of union-element
+	vals, err := p.parseValList()
+	if err != nil {
+		return OpError, nil, err
+	}
+	// distinguish cases
+	return OpUnion, vals, nil
+}
+
+// element ("," element)*
+// where element ::= union-element | subscript-expression,
+// where the latter cannot appear in a list.
+func (p *parser) parseValList() ([]Val, error) {
+	vals := []Val{}
 	for {
-		lx := p.lexPath()
-		switch lx.tok {
-
-		case tokError:
-			return OpError, "", lx.err
-		case tokEOF:
-			return OpError, "", p.unexpectedEOF()
-
-		// subscript-expression
-		case '*':
-			return OpWild, nil, nil
-		case '(':
-			e, err := p.parseExpr()
-			// need to lookahead for ":", the "expr" case of "start" (ie, it's a slice)
-			return OpExp, e, err
-		case tokFilter:
-			e, err := p.parseExpr()
-			return OpFilter, e, err
-
-		// union-element ("," union-element)
-		case tokInt:
-			// integer or start element of slice
-			// need to lookahead for ":" (OpIndex vs OpSlice)
-			union = append(union, &Step{OpIndex, []Val{lx.val}})
-		case tokString:
-			// string-literal
-			union = append(union, &Step{OpString, []Val{lx.val}})
-		case tokID:
-			// treat same as string-literal
-			union = append(union, &Step{OpId, []Val{lx.val}})
-
-		// error
-		default:
-			return OpError, "", fmt.Errorf("unexpected %v at %s", lx.tok, p.offset())
+		exp, err := p.parseVal()
+		if err != nil {
+			return nil, err
 		}
+		vals = append(vals, exp)
 		if p.lookPath() != ',' {
 			break
 		}
 		_ = p.lexPath()
 	}
-	return OpUnion, union, nil
+	return vals, nil
+}
+
+// union-element ::=  array-index | string-literal | array-slice   // could include identifier?
+// array-index ::= signed-integer
+// array-slice ::= start? ":" end? (":" stride?)?
+func (p *parser) parseVal() (*Step, error) {
+	lx := p.lexPath()
+	if lx.err != nil {
+		return nil, lx.err
+	}
+	switch lx.tok {
+	case tokError:
+		return nil, lx.err
+	case tokEOF:
+		return nil, p.unexpectedEOF()
+
+	// subscript-expression
+	case '*':
+		return &Step{OpWild, nil}, nil
+	case '(':
+		e, err := p.parseExpr()
+		if err != nil {
+			return nil, err
+		}
+		if p.lookPath() == ':' {
+			p.lexPath()
+			return p.parseSlice(e)
+		}
+		return &Step{OpExp, []Val{e}}, nil
+	case ':':
+		// slice with missing start value
+		return p.parseSlice(nil)
+
+	case tokFilter:
+		e, err := p.parseExpr()
+		if err != nil {
+			return nil, err
+		}
+		return &Step{OpFilter, []Val{e}}, nil
+
+	// definitely union-element
+	case tokInt:
+		// integer or start element of slice
+		// need to lookahead for ":" (OpIndex vs OpSlice)
+		if p.lookPath() == ':' {
+			p.lexPath()
+			return p.parseSlice(lx.val.(int64))
+		}
+		return &Step{OpInt, []Val{lx.val}}, nil
+	case tokString:
+		// string-literal
+		return &Step{OpString, []Val{lx.val}}, nil
+	case tokID:
+		// treat same as string-literal
+		return &Step{OpId, []Val{lx.val}}, nil
+
+	default:
+		// illegal
+		return nil, fmt.Errorf("unexpected %v at %s", lx.tok, p.offset())
+	}
+}
+
+// array-slice ::= start? ":" end? (":" stride?)?
+// The initial start? ":" has been consumed, the ":" alerting us to a slice.
+// The context means that valid successors are end, ":" (if a stride), "]", "," (if a union with a slice, and no stride),
+// and then "]" if there's no stride, or there's a stride expression.
+func (p *parser) parseSlice(start Val) (*Step, error) {
+	vals := []Val{start}
+	tok := p.lookPath()
+	if tok == ',' || tok == ']' {
+		// neither end nor stride, ie x[s:], make them empty
+		vals = append(vals, nil, nil)
+		return &Step{OpSlice, vals}, nil
+	}
+	// end? (":" stride?)?
+	if tok != ':' {
+		// must be end (":" stride?)?
+		e, err := p.parseSliceVal()
+		if err != nil {
+			return nil, err
+		}
+		vals = append(vals, e)
+	}
+	// (":" stride?)?
+	switch p.lookPath() {
+	case ']', ',':
+		vals = append(vals, nil)	// missing stride
+		return &Step{OpSlice, vals}, nil
+	case ':':
+		// ":" stride?
+		p.lexPath()
+		tok = p.lookPath()
+		if tok == ',' || tok == ']' {
+			vals = append(vals, nil)	// missing stride
+			return &Step{OpSlice, vals}, nil
+		}
+		e, err := p.parseSliceVal()
+		if err != nil {
+			return nil, err
+		}
+		vals = append(vals, e)
+		return &Step{OpSlice, vals}, nil
+	default:
+		return nil, fmt.Errorf("Xunexpected token %s at %s", p.lookPath(), p.offset())
+	}
+}
+
+// (end|stride) ::= signed-integer | expr
+func (p *parser) parseSliceVal() (Val, error) {
+	switch lx := p.lexPath(); lx.tok {
+	case '(':
+		return p.parseExpr()
+	case tokInt:
+		return lx.val.(int64), nil
+	default:
+		return nil, fmt.Errorf("unexpected %v at %s", lx.tok, p.offset())
+	}
 }
 
 // member ::= "*" | identifier | expr | integer
