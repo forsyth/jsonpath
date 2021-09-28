0cd65c0ccc51b108fdb122c0daf73fec8f2d2673
Charles Forsyth <charles.forsyth@gmail.com>
2021-09-28 17:12:24 +0100

    split Path definition from its parser

--- parsepath.go
+++ parsepath.go
@@ -3,45 +3,7 @@ package JSONPath
 import (
 	"errors"
 	"fmt"
-	"strings"
 )
-
-// Val is an int64, float64, string (literal or identifier), *Slice or Expr
-type Val interface{}
-
-// Path is a sequence of Steps, following the grammar.
-// The first step is always OpRoot.
-type Path []*Step
-
-// Step represents a single step in the path.
-type Step struct {
-	Op   Op	// Op is the action to take at this step. Not all Ops are valid Steps (eg, expression operators).
-	Args []Val	// Zero or more arguments to the operation (eg, integer and string values, an identifier, a Slice or a filter or other Expr).
-}
-
-func (step *Step) String() string {
-	var sb strings.Builder
-	sb.WriteString(step.Op.GoString())
-	if len(step.Args) > 0 {
-		sb.WriteByte('(')
-		for i, a := range step.Args {
-			if i > 0 {
-				sb.WriteString(", ")
-			}
-			sb.WriteString(fmt.Sprintf("%#v", a))
-		}
-		sb.WriteByte(')')
-	}
-	return sb.String()
-}
-
-// Slice is a Val that represents a JavaScript slice with [start: end: stride], where any of them might be optional.
-// It appears as an operand in an OpUnion.
-type Slice struct {
-	Start  Val	// optional starting offset
-	End    Val	// optional end offset (exclusive)
-	Stride Val	// optional value selecting every n array elements.
-}
 
 // path ::= "$" step*
 // step ::= "." member | ".." member | "[" subscript "]" | ".." "[" subscript "]"
@@ -176,6 +138,9 @@ func (p *parser) parseSubscript() (*Step, error) {
 			}
 			args = append(args, step.Args[0])
 		}
+		if len(args) == 1 {
+			return &Step{OpSelect, args}, nil
+		}
 		return &Step{OpUnion, args}, nil
 	}
 }
@@ -195,7 +160,7 @@ func (p *parser) parseValList() ([]*Step, error) {
 		if p.lookPath() != ',' {
 			break
 		}
-		_ = p.lexPath()
+		p.lexPath()
 	}
 	return vals, nil
 }
