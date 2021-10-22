40fe1c805a7c8a475718e448c1ac929e226001eb
Charles Forsyth <charles.forsyth@gmail.com>
2021-10-22 18:24:58 +0100

    update TODO

--- json.go
+++ json.go
@@ -6,11 +6,13 @@ type JSON = interface{}
 
 // enquiry functions on JSON, sometimes easier to read than type switches
 
+// isString returns true if v is a string.
 func isString(v JSON) bool {
 	_, ok := v.(string)
 	return ok
 }
 
+// isInt returns true if v is an integer (int might arise from len results).
 func isInt(v JSON) bool {
 	switch v.(type) {
 	case int, int64:
@@ -20,16 +22,19 @@ func isInt(v JSON) bool {
 	}
 }
 
+// isFloat returns true if v is a floating-point value.
 func isFloat(v JSON) bool {
 	_, ok := v.(float64)
 	return ok
 }
 
+// isSlice returns true if v represents slice parameters.
 func isSlice(v JSON) bool {
 	_, ok := v.(*Slice)
 	return ok
 }
 
+// isArith returns true if v is an arithmetic type in the JS sense.
 func isArith(v JSON) bool {
 	switch v.(type) {
 	case int, int64, float64:
@@ -41,6 +46,7 @@ func isArith(v JSON) bool {
 	}
 }
 
+// isSimple returns true if the v is "simple" (isn't a JSON object or array).
 func isSimple(v JSON) bool {
 	switch v.(type) {
 	case bool, int, int64, float64, string:
